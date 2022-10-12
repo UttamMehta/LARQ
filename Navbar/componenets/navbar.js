a0df82d49eadaf55931441e7dc208ed64645e89c
prachi301s <prachisahu2711@gmail.com>
2022-10-12 12:36:07 +0530

    navbar created

---
+++ Navbar/componenets/navbar.js
@@ -0,0 +1,21 @@
+function navbar(){
+    return `    <div id="nav">
+    <div id="logo"><img src="https://res.cloudinary.com/larq/image/upload/v1570724754/assets/spa/icons/logo-larq.svg" alt=""></div>
+</div>
+<div id="item">
+    <div> <a href="products.html">Products</a> </div>
+    <div> <a href="technology.html">Technology</a></div>
+    <div> <a href="signin.html">Sign In</a></div>
+    <div>
+    <select name="" id="selectLang">
+        <option value="en">EN <i class="fa-light fa-globe"></i></option>    
+        <option value="english">English</option>
+        <option value="french">French</option>
+    <option value="german">German</option>
+</select>
+</div>
+
+<div><i class="fa-light fa-cart-shopping"></i></div>
+</div>`;
+}
+export default navbar;
---
+++ Navbar/scripts/bottom.js
@@ -0,0 +1,2 @@
+import bottom from "../componenets/bottom.js";
+document.getElementById("main").innerHTML=bottom();
