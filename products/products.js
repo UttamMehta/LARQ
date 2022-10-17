dcb108dc3f6159dc45e537ff7f98bb52e9efd62b
nagendrachoudhary <nagendradangi105@gmail.com>
2022-10-17 19:14:51 +0530

    connect pages

--- products/products.js
+++ products/products.js
@@ -124,6 +124,22 @@ function fun6(){
        location.href="../cart/pages/detailPage.html"
 }
 
-function logo(){
-    location.href="../index.html"
+function logo() {
+    location.href = "../index.html"
+}
+function products() {
+    console.log("fsdhfkjhsd");
+    document.getElementById("products").href = "../products/products.html";
+}
+function technology() {
+    console.log("fsdhfkjhsd");
+    document.getElementById("technology").href = "../Technology/techno.html";
+}
+function signin() {
+    console.log("fsdhfkjhsd");
+    document.getElementById("signin").href = "../signin/signin.html";
+}
+function gotocart() {
+    console.log("fsdhfkjhsd");
+    document.getElementById("gotocart").href = "../cart/pages/cart.html";
 }
