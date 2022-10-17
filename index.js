58c475be1d57cc03dece286a2fc01656b0eea6fd
nagendrachoudhary <nagendradangi105@gmail.com>
2022-10-17 19:05:18 +0530

    index

--- index.js
+++ index.js
@@ -231,3 +231,15 @@ function products() {
   console.log("fsdhfkjhsd");
   document.getElementById("products").href = "./products/products.html";
 }
+function technology() {
+  console.log("fsdhfkjhsd");
+  document.getElementById("technology").href = "./Technology/techno.html";
+}
+function signin() {
+  console.log("fsdhfkjhsd");
+  document.getElementById("signin").href = "./signin/signin.html";
+}
+function gotocart() {
+  console.log("fsdhfkjhsd");
+  document.getElementById("gotocart").href = "./cart/pages/cart.html";
+}
